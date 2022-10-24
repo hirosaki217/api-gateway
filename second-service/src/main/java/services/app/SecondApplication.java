974a673b7c49f34e44c4677c1b036588a55bdcd6
hirosaki217 <hirosaki217@gmail.com>
2022-10-24 14:10:49 +0700

    api-gateway

---
+++ second-service/src/main/java/services/app/SecondApplication.java
@@ -0,0 +1,12 @@
+package services.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondApplication.class, args);
+	}
+}
