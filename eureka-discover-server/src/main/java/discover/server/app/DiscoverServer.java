9032cb23e4e0d04bc823dd15f9dc4390d10fbce7
hirosaki217 <hirosaki217@gmail.com>
2022-10-29 21:38:00 +0700

    tao eureka discover server

---
+++ eureka-discover-server/src/main/java/discover/server/app/DiscoverServer.java
@@ -0,0 +1,14 @@
+package discover.server.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoverServer {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DiscoverServer.class, args);
+	}
+}
